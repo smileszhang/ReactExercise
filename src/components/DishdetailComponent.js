5dff574022d4d6bd06b21c4f75e6295f5f0103c5
smileszhang <shiyang.zhang@hotmail.com>
2018-11-29 16:39:52 -0800

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-    renderDish(dish) {
+    function RenderDish({dish}) {
             return(
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -15,7 +14,7 @@ class DishDetail extends Component {
             );
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         const comment = comments.map((commentDish) => {
             return(
                 <div>
@@ -34,8 +33,9 @@ class DishDetail extends Component {
         );
     }
     
-    render(){
-        const dish = this.props.dish;
+    const DishDetail = (props) => {
+
+        const dish = props.dish;
         if (dish == null) {
             return (
                 <div></div>
@@ -46,16 +46,15 @@ class DishDetail extends Component {
                 <div class='container'>
                 <div className='row'>
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(dish)}
+                        <RenderDish dish={dish}/>
                     </div>
                     <div  className="col-12 col-md-5 m-1">
                         <h4>Comments</h4>
-                        {this.renderComments(dish.comments)}
+                        <RenderComments comments={dish.comments}/>
                     </div>
                 </div>
                 </div>
             );
-}
 }
 
 
